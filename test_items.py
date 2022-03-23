9c5ebdd9e92a534a96506bc9f59caaa7430d9750
makxnl <96685842+makxnl@users.noreply.github.com>
2022-03-23 21:51:23 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,1 +1,10 @@
+import time
+from selenium.webdriver.common.by import By
 
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_button_on_page(browser):
+    browser.get(link)
+    time.sleep(30)
+    btn = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
+    assert btn, "Button not find"

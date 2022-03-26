e317dcd750dbcc0078616265c25180f1f5147f2f
makxnl <96685842+makxnl@users.noreply.github.com>
2022-03-26 15:55:26 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_button_on_page(browser):
+def test_add_button_on_page(browser):
     browser.get(link)
     time.sleep(30)
     btn = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
